c2b4eb79b2995591ed5fc9553f7ae6854a11aeef
Alexander Bernhardt <abe@maltego.com>
2025-02-18 10:00:03 +0100

    Remove logs

---
+++ .changeset/nasty-bats-punch.md
@@ -0,0 +1,5 @@
+---
+"tools": minor
+---
+
+Remove logs
--- packages/tools/src/hooks/useCounter.tsx
+++ packages/tools/src/hooks/useCounter.tsx
@@ -3,8 +3,6 @@ import React, { useCallback } from 'react'
 export const useCounter = () => {
   const [count, setCount] = React.useState(0)
 
-  console.log("count: ", count)
-
   const increment = useCallback(() => {
     setCount((prevCount) => prevCount + 1)
   }, [])
