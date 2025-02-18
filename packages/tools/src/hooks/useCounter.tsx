27234247ea413d7977661a7b9d4d8cbe6ad0db91
Alexander Bernhardt <abe@maltego.com>
2025-02-18 09:55:13 +0100

    Add logs to the hook

--- packages/tools/src/hooks/useCounter.tsx
+++ packages/tools/src/hooks/useCounter.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react'
 
 export const useCounter = () => {
   const [count, setCount] = React.useState(0)
-
+  console.log("count: ", count)
   const increment = useCallback(() => {
     setCount((prevCount) => prevCount + 1)
   }, [])
