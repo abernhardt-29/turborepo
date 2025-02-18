2b4a24d5bd5c8ced0f829b3b015448f89ccc2007
Alexander Bernhardt <abe@maltego.com>
2025-02-18 09:07:13 +0100

    Add className to the CustomButton

--- packages/custom-ui/src/components/CustomButton.tsx
+++ packages/custom-ui/src/components/CustomButton.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 const CustomButton: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-
-  return <button>{ children }</button>
+  return <button className="custom-button">{ children }</button>
 }
 
 export default CustomButton
