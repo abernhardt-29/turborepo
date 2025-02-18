2b9b58e0e6ba7bf23304b885c2a0f9276c2a4341
Alexander Bernhardt <abe@maltego.com>
2025-02-18 09:55:50 +0100

    Format code

--- packages/tools/src/hooks/useCounter.tsx
+++ packages/tools/src/hooks/useCounter.tsx
@@ -2,7 +2,9 @@ import React, { useCallback } from 'react'
 
 export const useCounter = () => {
   const [count, setCount] = React.useState(0)
+
   console.log("count: ", count)
+
   const increment = useCallback(() => {
     setCount((prevCount) => prevCount + 1)
   }, [])
